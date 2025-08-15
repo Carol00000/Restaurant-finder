f988f3dad6a1707b2f5598fbb56b569e1aba6dff
Carol00000 <caroloppuer03@gmail.com>
2025-08-15 17:06:51 +0800

    Update start command and fix routes for Railway development

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: proc { [200, {}, ['OK']] }
+  root 'application#health'
   #root 'dashboard#profile'
 
   get 'restaurants/search', to: 'restaurants#search', as: 'restaurants_search'
